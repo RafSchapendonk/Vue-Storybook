860bb596d0c78820f800743a2f7c763759c36aa2
Raf Schapendonk <rschapendonk@rb-media.nl>
2022-03-09 10:58:04 +0100

    installed accessibility addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -38,6 +38,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@pbutlewski/storybook-html",
+    '@storybook/addon-a11y',
     require.resolve('storybook-addon-grid/preset'),
     {
       name: '@storybook/addon-postcss',
